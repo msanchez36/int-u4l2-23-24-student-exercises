15ad2db0d6ba3db30e769b3c5f2dd721f4b5b780
msanchez36 <144706185+msanchez36@users.noreply.github.com>
2024-02-21 15:46:08 +0000

    Worked on string concatenation exercise number 1

--- script.js
+++ script.js
@@ -4,12 +4,13 @@ let captionOne = document.querySelector(".caption-one");
 buttonOne.addEventListener("click", function() {
     // 1. Declare a variable named spidyCaption.
     //  - Store the value of the input with a class of "comment-one".
-
+    let spidyCaption = document.querySelector(".comment-one").value;
 
     // 2. Display the variable spidyCaption to captionOne.
+    captionOne.innerHTML = spidyCaption;
 
-
-});
+});;
 
 // 3. Repeat the previous steps for the second meme.
 //  - Be sure to store variables for the button and caption.
+    captionTwo.innerHTML = kermitCaption;
